d5a943abbb43ae1bae5bc96e519c9621e358d2e1
Tulitas <tulitas1@gmail.com>
2021-03-16 22:46:20 +0200

    repayment

--- src/main/java/back/controllers/MLoanRepaymentScheduleController.java
+++ src/main/java/back/controllers/MLoanRepaymentScheduleController.java
@@ -6,7 +6,6 @@ import back.models.MLoanRepaymentSchedule;
 import back.repositories.MLoanRepaymentSheduleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -26,38 +25,42 @@ public class MLoanRepaymentScheduleController {
         return mLoanRepaymentSheduleRepository.findAll();
     }
 
-    @GetMapping("/mloan/{id}")
-    public ResponseEntity<MLoan> getMLoanById(@PathVariable(value = "id") Long mLoanId)
+    @GetMapping("/mloanrepayment/{id}")
+    public ResponseEntity<MLoanRepaymentSchedule> getMLoanRepaymentById(@PathVariable(value = "id") Long mLoanRepaymentId)
             throws ResourcesNotFoundException {
-        MLoan mLoan = mLoanRepository.findById(mLoanId)
-                .orElseThrow(() -> new ResourcesNotFoundException("Employee not found for this id :: " + mLoanId));
-        return ResponseEntity.ok().body(mLoan);
+        MLoanRepaymentSchedule mLoanRepaymentSchedule = mLoanRepaymentSheduleRepository.findById(mLoanRepaymentId)
+                .orElseThrow(() -> new ResourcesNotFoundException("Employee not found for this id :: " + mLoanRepaymentId));
+        return ResponseEntity.ok().body(mLoanRepaymentSchedule);
     }
 
-    @PostMapping("/mloan")
-    public MLoan createMLoan(@Valid @RequestBody MLoan mLoan) {
-        return mLoanRepository.save(mLoan);
+    @PostMapping("/mloanrepayment")
+    public @Valid MLoanRepaymentSchedule createMLoanRepayment(@Valid @RequestBody MLoanRepaymentSchedule mLoanRepaymentSchedule) {
+        return mLoanRepaymentSheduleRepository.save(mLoanRepaymentSchedule);
     }
 
-    @PutMapping("/mloan/{id}")
-    public ResponseEntity<MLoan> updateMLoan(@PathVariable(value = "id") Long mLoanId,
-                                             @Valid @RequestBody MLoan mLoanDetails) throws ResourcesNotFoundException {
-        MLoan mLoan = mLoanRepository.findById(mLoanId)
-                .orElseThrow(() -> new ResourcesNotFoundException("Employee not found for this id :: " + mLoanId));
+    @PutMapping("/mloanrepayment/{id}")
+    public ResponseEntity<MLoanRepaymentSchedule> updateMLoanRepayment(@PathVariable(value = "id") Long mLoanRepaymentScheduleId,
+                                             @Valid @RequestBody MLoanRepaymentSchedule mLoanRepaymentScheduleDetails) throws ResourcesNotFoundException {
+        MLoanRepaymentSchedule mLoanRepaymentSchedule = mLoanRepaymentSheduleRepository.findById(mLoanRepaymentScheduleId)
+                .orElseThrow(() -> new ResourcesNotFoundException("Employee not found for this id :: " + mLoanRepaymentScheduleId));
 
-        mLoan.setAccount_no(mLoanDetails.getAccount_no());
-        mLoan.setDisbursedon_date(mLoanDetails.getDisbursedon_date());
-        final MLoan updatedMLoan = mLoanRepository.save(mLoan);
-        return ResponseEntity.ok(updatedMLoan);
+        mLoanRepaymentSchedule.setCompleted_derived(mLoanRepaymentScheduleDetails.getCompleted_derived());
+        mLoanRepaymentSchedule.setDuedate(mLoanRepaymentScheduleDetails.getDuedate());
+        mLoanRepaymentSchedule.setFee_charges_amount(mLoanRepaymentScheduleDetails.getFee_charges_amount());
+        mLoanRepaymentSchedule.setInterest_amount(mLoanRepaymentScheduleDetails.getInterest_amount());
+        mLoanRepaymentSchedule.setPanalty_charges_amount(mLoanRepaymentScheduleDetails.getPanalty_charges_amount());
+        mLoanRepaymentSchedule.setPrincipal_amount(mLoanRepaymentScheduleDetails.getPrincipal_amount());
+        final MLoanRepaymentSchedule updatedMLoanRepayment = mLoanRepaymentSheduleRepository.save(mLoanRepaymentSchedule);
+        return ResponseEntity.ok(updatedMLoanRepayment);
     }
 
-    @DeleteMapping("/mloan/{id}")
-    public Map<String, Boolean> deleteMLoan(@PathVariable(value = "id") Long mLoanId)
+    @DeleteMapping("/mloanrepayment/{id}")
+    public Map<String, Boolean> deleteMLoanReapyment(@PathVariable(value = "id") Long mLoanRepaymentScheduleId)
             throws ResourcesNotFoundException {
-        MLoan mLoan = mLoanRepository.findById(mLoanId)
-                .orElseThrow(() -> new ResourcesNotFoundException("Employee not found for this id :: " + mLoanId));
+        MLoanRepaymentSchedule mLoanRepaymentSchedule = mLoanRepaymentSheduleRepository.findById(mLoanRepaymentScheduleId)
+                .orElseThrow(() -> new ResourcesNotFoundException("Employee not found for this id :: " + mLoanRepaymentScheduleId));
 
-        mLoanRepository.delete(mLoan);
+        mLoanRepaymentSheduleRepository.delete(mLoanRepaymentSchedule);
         Map<String, Boolean> response = new HashMap<>();
         response.put("deleted", Boolean.TRUE);
         return response;
